bc881950d0a7af20c2077e3e08f81456ad6210b5
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2023-10-26 20:08:21 +0200

    Adding tests for Hamburger component

---
+++ __tests__/components/atoms/Hamburger.test.tsx
@@ -0,0 +1,33 @@
+import Hamburger from "@/components/atoms/hamburger/Hamburger";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+let isMenuOpen = false;
+const setIsMenuOpen = () => {
+  isMenuOpen = !isMenuOpen;
+};
+
+describe('Hamburger component', () => {
+  it('correctly renders the component', () => {
+    render(<Hamburger isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />)
+    expect(screen.getByTestId('hamburger')).toBeInTheDocument();
+  })
+
+  it('toggles isMenuOpen state on click', () => {
+    render(<Hamburger isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />)
+    const hamburger = screen.getByTestId('hamburger');
+
+    fireEvent.click(hamburger);
+
+    expect(isMenuOpen).toBe(true);
+  })
+
+  it('changes appearance when menu is open', () => {
+    render(<Hamburger isMenuOpen={true} setIsMenuOpen={setIsMenuOpen} />);
+
+    const firstLine = screen.getByTestId('first-line');
+    const secondLine = screen.getByTestId('second-line');
+
+    expect(firstLine).toHaveClass('translate-y-[0.2rem] rotate-45');
+    expect(secondLine).toHaveClass('-translate-y-[0.2rem] -rotate-45 w-full');
+  });
+});
--- components/atoms/hamburger/Hamburger.tsx
+++ components/atoms/hamburger/Hamburger.tsx
@@ -2,9 +2,9 @@ import { MobileMenuOpen } from "@/models/models"
 
 const Hamburger = ({ isMenuOpen, setIsMenuOpen }: MobileMenuOpen) => {
   return (
-    <div className="flex flex-col justify-between gap-1 w-5 cursor-pointer z-10 md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-      <div className={`w-full h-[0.125rem] bg-darkBlue transition-all ease-in ${isMenuOpen ? 'translate-y-[0.2rem] rotate-45' : ''}`} />
-      <div className={`h-[0.125rem] bg-darkBlue transition-all ease-in ${isMenuOpen ? '-translate-y-[0.2rem] -rotate-45 w-full' : 'w-[70%]'}`} />
+    <div className="flex flex-col justify-between gap-1 w-5 cursor-pointer z-10 md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)} data-testid="hamburger">
+      <div className={`w-full h-[0.125rem] bg-darkBlue transition-all ease-in ${isMenuOpen ? 'translate-y-[0.2rem] rotate-45' : ''}`} data-testid="first-line" />
+      <div className={`h-[0.125rem] bg-darkBlue transition-all ease-in ${isMenuOpen ? '-translate-y-[0.2rem] -rotate-45 w-full' : 'w-[70%]'}`} data-testid="second-line" />
     </div>
   )
 }
