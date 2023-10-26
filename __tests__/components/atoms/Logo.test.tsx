4e37b0c03c235029fdad26d47beeb88e24224d9c
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2023-10-26 19:42:36 +0200

    Adding better tests for Logo component

--- __tests__/components/atoms/Logo.test.tsx
+++ __tests__/components/atoms/Logo.test.tsx
@@ -1,20 +1,21 @@
 import Logo from "@/components/atoms/logo/Logo";
 import { render, screen } from "@testing-library/react";
 
-it('Correctly renders the Logo component', () => {
-  render(<Logo />);
+describe('Logo component', () => {
+  it('Correctly renders the Logo component', () => {
+    render(<Logo />);
+    const logo = screen.getByText('Frontnerd');
 
-  const logo = screen.getByText('Frontnerd');
+    expect(logo).toBeInTheDocument()
 
-  expect(logo).toBeInTheDocument()
+  });
 
-});
+  it('Leads to the correct url', () => {
+    render(<Logo />);
+    const logoLink = screen.getByRole('link');
 
-it('Leads to the correct url', () => {
-  render(<Logo />);
+    expect(logoLink).toHaveAttribute("href", "/");
 
-  const logoLink = screen.getByText('Frontnerd');
+  });
+})
 
-  expect(logoLink).toHaveAttribute("href", "/");
-
-});
