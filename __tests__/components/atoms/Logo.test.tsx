ebbd3fafc3ab7e7f9bc2d5a6058b90edd70e12a5
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2023-11-27 13:29:52 +0100

    Adding tests for logo component

---
+++ __tests__/components/atoms/Logo.test.tsx
@@ -0,0 +1,20 @@
+import Logo from "@/components/atoms/logo/Logo";
+import { getByRole, render, screen } from "@testing-library/react";
+
+it('Correctly renders the Logo component', () => {
+  render(<Logo />);
+
+  const logo = screen.getByText('Frontnerd');
+
+  expect(logo).toBeInTheDocument()
+
+});
+
+it('Leads to the correct url', () => {
+  render(<Logo />);
+
+  const logoLink = screen.getByText('Frontnerd');
+
+  expect(logoLink).toHaveAttribute("href", "/");
+
+});
